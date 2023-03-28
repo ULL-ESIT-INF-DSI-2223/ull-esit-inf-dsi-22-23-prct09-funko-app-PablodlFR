d3d08483f1c74334442e659222ea2d23fb261acf
PablodlFR <alu0101336152@ull.edu.es>
2023-03-28 16:01:43 +0000

    update

--- src/types.ts
+++
@@ -1,3 +0,0 @@
-export type ColorType = 'Amarillo' | 'Azul' | 'Rojo' | 'Verde';
-export type PokemonType = 'Eléctrico' | 'Fuego' | 'Agua' | 'Planta';
-export type EvolutionType = '1ª' | '2ª' | '3ª';
